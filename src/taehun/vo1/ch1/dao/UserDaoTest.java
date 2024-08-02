27292a7a7c8425e70ecf2e48fc0918ab0079d103
taehun187 <lth1518@gmail.com>
2024-08-03 01:15:16 +0900

    Create UserDaoTest.java

---
+++ src/taehun/vo1/ch1/dao/UserDaoTest.java
@@ -0,0 +1,32 @@
+package vol1.taehun.ch1.dao;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import vol1.taehun.ch1.domain.User;
+
+public class UserDaoTest {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("dataSource");
+		user.setName("taehun");
+		user.setPassword("mariaDB");
+
+		dao.add(user);
+			
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+			
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+}
